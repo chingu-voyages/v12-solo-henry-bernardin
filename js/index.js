0645ac00e96b8d91b26ad5b9731d32ac681c6bc0
Henry-Bernardin <henry.bernardin@gmail.com>
2019-11-10 00:23:32 -0500

    Box shadow effect added to nav links container

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.body.scrollTop > 45 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 44 || document.documentElement.scrollTop > 50) {
     document.getElementById("nav-links-purple").id = "nav-links-white";
   } else {
     document.getElementById("nav-links-white").id = "nav-links-purple";
